28de7ea509460f98a2167ce4ca0c8dbf65da0cb6
giuseppesciacca <sciaccagi@gmail.com>
2023-09-09 10:56:01 +0200

    ADD fontAwesome icon for dice

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,3 +1,11 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDiceOne } from "@fortawesome/free-solid-svg-icons";
+import { faDiceTwo } from "@fortawesome/free-solid-svg-icons";
+import { faDiceThree } from "@fortawesome/free-solid-svg-icons";
+import { faDiceFour } from "@fortawesome/free-solid-svg-icons";
+import { faDiceFive } from "@fortawesome/free-solid-svg-icons";
+import { faDiceSix } from "@fortawesome/free-solid-svg-icons";
+
 export default function Die({ value, holdDice, isHeld }) {
 
     /**
@@ -5,17 +13,38 @@ export default function Die({ value, holdDice, isHeld }) {
      * @param {number} value 
      */
     function nDot(value) {
-        const nDice = Array.from({ length: value });
 
-        return nDice.map(() => <span className="dot"></span>)
+        let icon = null;
+
+        switch (value) {
+            case 1:
+                icon = faDiceOne;
+                break;
+            case 2:
+                icon = faDiceTwo;
+                break;
+            case 3:
+                icon = faDiceThree;
+                break;
+            case 4:
+                icon = faDiceFour;
+                break;
+            case 5:
+                icon = faDiceFive;
+                break;
+            case 6:
+                icon = faDiceSix;
+                break;
+        }
+
+        return <FontAwesomeIcon icon={icon} size="2xl" style={{ color: "#000000" }} />;
     }
 
     return (
         <>
-            <div className={`die rounded-2 ${isHeld && "die__active"} `} onClick={holdDice}>
+            <div className={`d-flex align-items-center justify-content-center die die_icon rounded-2 ${isHeld && "die__active"} `} onClick={holdDice}>
 
                 {nDot(value)}
-
             </div>
         </>
         /* /.die */
